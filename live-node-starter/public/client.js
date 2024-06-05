6a40f20310a29df6213c0b2c9e76ed65728c7f74
dza59 <zdn.zhang@gmail.com>
2024-06-05 11:06:47 -0700

    live transcribe tool starter demo use deepgram

---
+++ live-node-starter/public/client.js
@@ -0,0 +1,68 @@
+// /public/client.js
+
+const captions = window.document.getElementById('captions');
+
+async function getMicrophone() {
+  const userMedia = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+
+  return new MediaRecorder(userMedia);
+}
+
+async function openMicrophone(microphone, socket) {
+  await microphone.start(500);
+
+  microphone.onstart = () => {
+    console.log('client: microphone opened');
+    document.body.classList.add('recording');
+  };
+
+  microphone.onstop = () => {
+    console.log('client: microphone closed');
+    document.body.classList.remove('recording');
+  };
+
+  microphone.ondataavailable = (e) => {
+    console.log('client: sent data to websocket');
+    socket.emit('packet-sent', e.data);
+  };
+}
+
+async function closeMicrophone(microphone) {
+  microphone.stop();
+}
+
+async function start(socket) {
+  const listenButton = document.getElementById('record');
+  let microphone;
+
+  console.log('client: waiting to open microphone');
+
+  listenButton.addEventListener('click', async () => {
+    if (!microphone) {
+      // open and close the microphone
+      microphone = await getMicrophone();
+      await openMicrophone(microphone, socket);
+    } else {
+      await closeMicrophone(microphone);
+      microphone = undefined;
+    }
+  });
+}
+
+window.addEventListener('load', () => {
+  const socket = io((options = { transports: ['websocket'] }));
+
+  socket.on('connect', async () => {
+    console.log('client: connected to websocket');
+    await start(socket);
+  });
+
+  socket.on('transcript', (transcript) => {
+    console.log('Received transcript:', transcript);
+    if (transcript) {
+      captions.innerHTML = `<span>${transcript}</span>`;
+    }
+  });
+});
---
+++ live-node-starter/server.js
@@ -0,0 +1,106 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { createClient, LiveTranscriptionEvents } = require('@deepgram/sdk');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const deepgramClient = createClient(process.env.DEEPGRAM_API_KEY);
+let keepAlive;
+
+const setupDeepgram = (socket) => {
+  const deepgram = deepgramClient.listen.live({
+    language: 'en',
+    punctuate: true,
+    smart_format: true,
+    model: 'nova',
+  });
+
+  if (keepAlive) clearInterval(keepAlive);
+  keepAlive = setInterval(() => {
+    console.log('deepgram: keepalive');
+    deepgram.keepAlive();
+  }, 10 * 1000);
+
+  deepgram.addListener(LiveTranscriptionEvents.Open, async () => {
+    console.log('deepgram: connected');
+
+    deepgram.addListener(LiveTranscriptionEvents.Close, async () => {
+      console.log('deepgram: disconnected');
+      clearInterval(keepAlive);
+      deepgram.finish();
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Error, async (error) => {
+      console.log('deepgram: error recieved');
+      console.error(error);
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Warning, async (warning) => {
+      console.log('deepgram: warning recieved');
+      console.warn(warning);
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Transcript, (data) => {
+      console.log('deepgram: packet received');
+      console.log('deepgram: transcript received');
+      const transcript = data.channel.alternatives[0].transcript ?? '';
+      console.log('socket: transcript sent to client');
+      socket.emit('transcript', transcript);
+      console.log('socket: transcript data sent to client');
+      socket.emit('data', data);
+    });
+
+    deepgram.addListener(LiveTranscriptionEvents.Metadata, (data) => {
+      console.log('deepgram: packet received');
+      console.log('deepgram: metadata received');
+      console.log('socket: metadata sent to client');
+      socket.emit('metadata', data);
+    });
+  });
+
+  return deepgram;
+};
+
+io.on('connection', (socket) => {
+  console.log('socket: client connected');
+  let deepgram = setupDeepgram(socket);
+
+  socket.on('packet-sent', (data) => {
+    console.log('socket: client data received');
+
+    if (deepgram.getReadyState() === 1 /* OPEN */) {
+      console.log('socket: data sent to deepgram');
+      deepgram.send(data);
+    } else if (deepgram.getReadyState() >= 2 /* 2 = CLOSING, 3 = CLOSED */) {
+      console.log("socket: data couldn't be sent to deepgram");
+      console.log('socket: retrying connection to deepgram');
+      /* Attempt to reopen the Deepgram connection */
+      deepgram.finish();
+      deepgram.removeAllListeners();
+      deepgram = setupDeepgram(socket);
+    } else {
+      console.log("socket: data couldn't be sent to deepgram");
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('socket: client disconnected');
+    deepgram.finish();
+    deepgram.removeAllListeners();
+    deepgram = null;
+  });
+});
+
+app.use(express.static('public/'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('listening on localhost:3000');
+});
